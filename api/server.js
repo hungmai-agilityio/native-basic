cabf5faa15df81d506063172b1f83192058c9466
Hung Mai <hung.mai@asnet.com.vn>
2025-02-15 16:35:31 +0700

    Use json server auth

--- api/server.js
+++ api/server.js
@@ -1,13 +1,18 @@
-// server.js
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
- 
-server.use(middlewares)
-server.use('', router)
-server.listen(process.env.PORT || 5000, () => {
-  console.log('JSON Server is running')
-})
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
-module.exports = server
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
+server.db = router.db;
+server.use(auth);
+server.use(router);
+
+server.listen(process.env.PORT || 5000, () => {
+  console.log("JSON Server is running");
+});
+
+module.exports = server;
